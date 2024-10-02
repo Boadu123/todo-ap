2d5be1044392de04580b4ff4461ce3a3fdb4117d
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-10-02 09:16:01 +0000

    commit delete and update

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 import express from "express";
+import todoRouter from "./routes/todo.js";
 
 // Create an express app
 const app = express();
 
-// Define routes
-app.get("/hello", (req, res, next) => {
-  console.log(req.headers);
-  res.json("You visited the hello endpoint!");
-});
-
-app.get("/goobye", function (req, res, next) {
-  console.log(req.query);
-  res.json("Same to you");
-});
+// Use routes
+app.use(todoRouter);
 
 // Listen for incoming request
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log("App is listening on port 3000");
 });
